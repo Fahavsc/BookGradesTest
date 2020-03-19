327ed2b85aedcefcd16f326d9b0daa97f0c1ae13
Fabricio Cirino <fabricioavsc@gmail.com>
2020-03-19 13:40:17 -0300

    Test unitario utilizando Assert do XUnit

---
+++ BookTests.cs
@@ -0,0 +1,32 @@
+
+using GradeBook;
+using System;
+using Xunit;
+
+namespace Gradebook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+            //act
+
+            var stats = book.GetStatistics();
+            book.ShowStatistics();
+
+
+            //assert
+            Assert.Equal(85.6, stats.Average, 1);
+            Assert.Equal(77.3, stats.Low, 1) ;
+            Assert.Equal(90.5, stats.High, 1);
+
+        }
+    }
+}
